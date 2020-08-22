88e27561bb54dcdfcc1232cab5ccfc9742cbe845
ganyulong02 <ganyulong02@gmail.com>
2020-08-22 11:12:07 -0700

    Add coupons and subscription

--- plans/views.py
+++ plans/views.py
@@ -5,6 +5,9 @@ from django.views import generic
 from .models import FitnessPlan
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+import stripe
+
+stripe.api_key = 'sk_test_51HHjtKJSwY2ShoTT2pNbqvdXwXjeXU7My9gauhKDxSB7zFLMxFc9oAjyxXMKtLvcCKzCEOkNtC68F3apBKfmTPYU005xszO2En'
 
 
 def home(request):
@@ -26,10 +29,55 @@ def join(request):
 
 @login_required
 def checkout(request):
+
+    coupons = {
+        'halloween': 31,
+        'welcome': 10,
+   }
+
     if request.method == 'POST':
+        stripe_customer = stripe.Customer.create(email=request.user.email, source=request.POST['stripeToken'])
+        plan = 'price_1HHk9WJSwY2ShoTTkf7Vxy8O' # monthly
+        if request.POST['plan'] == 'yearly':
+            plan = 'price_1HHkALJSwY2ShoTT1lvBuXvX'  # yearly
+        if request.POST['coupon'] in coupons:
+            percentage = coupons[request.POST['coupon'].lower()]
+            try:
+                coupon = stripe.Coupon.create(duration='once', id=request.POST['coupon'].lower(), percent_off=percentage)
+            except:
+                pass
+            subscription = stripe.Subscription.create(customer=stripe_customer.id, items=[{'plan': plan}],
+                                                      coupon=request.POST['coupon'].lower())
+        else:
+            subscription = stripe.Subscription.create(customer=stripe_customer.id, items=[{'plan': plan}])
         return redirect('home')
     else:
-        return render(request, 'plans/checkout.html')
+        # request.method == 'GET'
+        plan = 'monthly'
+        coupon = 'none'
+        price = 1000  # in penny unit
+        og_dollar = 10
+        coupon_dollar = 0
+        final_dollar = 10
+        if request.method == 'GET' and 'plan' in request.GET:
+            if request.GET['plan'] == 'yearly':
+                plan = 'yearly'
+                price = 10000
+                og_dollar = 100
+                final_dollar = 100
+
+        if request.method == 'GET' and 'coupon' in request.GET:
+            if request.GET['coupon'].lower() in coupons:
+                coupon = request.GET['coupon'].lower()
+                percentage = coupons[coupon]
+                coupon_price = int((percentage / 100) * price)
+                price = price - coupon_price
+                coupon_dollar = str(coupon_price)[:-2] + '.' + str(coupon_price)[-2:]  # transform cent to dollar
+                final_dollar = str(price)[:-2] + '.' + str(price)[-2:]
+
+        return render(request, 'plans/checkout.html', {'plan': plan, 'coupon': coupon, 'price': price,
+                                                       'og_dollar': og_dollar, 'coupon_dollar': coupon_dollar,
+                                                       'final_dollar': final_dollar})
 
 
 def settings(request):
